a68f855182826883336b6987b81ec57a2e087af9
donglxf <donglxf@gmail.com>
2018-03-28 22:47:51 +0800

    jpa

---
+++ boot-jap/src/main/java/com/example/jap/bootjap/BootJapApplication.java
@@ -0,0 +1,12 @@
+package com.example.jap.bootjap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJapApplication.class, args);
+	}
+}
